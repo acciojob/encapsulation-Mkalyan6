97eb44eb92fac360b088ce380a1e0816cb2bad00
Mkalyan6 <120394363+Mkalyan6@users.noreply.github.com>
2023-08-21 22:03:04 +0530

    Data Encapsulation example

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] Args){
+      RWOnly obj=new RWOnly();
+//      obj.name="kalyan";
+//     private member has no access to modifiy or create datat to variable as it is private
+      obj.setName("kalyan");
+      obj.getName();
+  }
 }
